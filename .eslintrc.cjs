1b8cedfc7f29a143571b8710413ea37bd281d768
novoseltcev <novoseltcev.stanislav@gmail.com>
2023-01-19 18:01:05 +0300

    feat(setup): add configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+const { configure, presets } = require('eslint-kit');
+
+module.exports = configure({
+  presets: [
+    presets.imports(),
+    presets.node(),
+    presets.prettier(),
+    presets.typescript(),
+    presets.react({
+      newJSXTransform: true,
+    }),
+    presets.effector(),
+  ],
+});
---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+};
