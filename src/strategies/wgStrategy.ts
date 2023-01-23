2ea9757a62150be2b48563cc235335d5a3a72dcb
novoseltcev <novoseltcev.stanislav@gmail.com>
2023-01-23 13:54:05 +0300

    chore(strategy): change abstraction

--- src/strategies/wgStrategy.ts
+++ src/strategies/wgStrategy.ts
@@ -1,20 +1,38 @@
+import { run } from '../utils';
 import { WgConfig } from 'wgConfig';
 
+type WgResponse = {
+  stdout?: string;
+  stderr?: string;
+}
+
 export abstract class WgStrategy {
+  private processName: string
+
+  constructor(processName: string) {
+    this.processName = processName;
+  }
+
+  protected async exec(command: string, sudoPrompt = true): Promise<WgResponse> {
+    const result = await run(command, this.processName, sudoPrompt);
+    if (result.stderr && Buffer.isBuffer(result.stderr)) {
+      result.stderr = result.stderr.toString('utf-8');
+    }
+    if (result.stdout && Buffer.isBuffer(result.stdout)) {
+      result.stdout = result.stdout.toString('utf-8');
+    }
+    return {
+      stdout: result.stdout,
+      stderr: result.stderr,
+    };
+  }
+
   abstract isInstalled(): Promise<boolean>;
 
-  abstract save(config: WgConfig, dir: string): Promise<void>;
-  abstract up(device: string, dir: string): Promise<void>;
-  abstract down(device: string): Promise<void>;
+  abstract getActiveDevice(): Promise<string>;
+  abstract up(filePath: string): Promise<void>;
+  abstract down(filePath: string): Promise<void>;
   abstract status(device: string): Promise<boolean>;
-
-  restart(device: string, dir: string): Promise<void> {
-    return new Promise((resolve, _) => {
-      this.down(device)
-        .finally(() => this.up(device, dir))
-        .then(() => resolve());
-    });
-  }
 
   abstract getPublicKey(config: string): Promise<string>;
 }
