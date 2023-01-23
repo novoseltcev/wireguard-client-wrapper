3a75907ee89aa08bb7009dd211a6dc7b498bce47
novoseltcev <novoseltcev.stanislav@gmail.com>
2023-01-23 13:54:40 +0300

    chore(strategy): rewrite linux strategy

--- src/strategies/wgLinuxStrategy.ts
+++ src/strategies/wgLinuxStrategy.ts
@@ -1,88 +1,88 @@
-import { exec } from 'child_process';
-import { WgConfig } from 'wgConfig';
+import { ExecError } from '../utils';
+
 import { WgStrategy } from './wgStrategy';
 
 export class WgLinuxStrategy extends WgStrategy {
-  isInstalled(): Promise<boolean> {
+  async isInstalled(): Promise<boolean> {
     const wgCommand = 'wg --version';
     const wgQuickCommand = 'wg-quick --version';
-    return new Promise((resolve, reject) => {
-      exec(wgCommand, (error, _, stderr) => {
-        if (error) {
-          reject(error);
+    const { stderr } = await this.exec(wgCommand, false);
+    if (stderr) {
+      return false;
+    }
+
+    try {
+      await this.exec(wgQuickCommand, false);
+      return false;
+    } catch (error) {
+      if (error instanceof ExecError) {
+        return String(error.stderr).includes('command not found') ? false : true;
+      }
+      throw error;
+    }
+  }
+
+  async getActiveDevice(): Promise<string> {
+    try {
+      const {stderr, stdout} = await this.exec("wg show", false);
+      if (stderr) {
+        throw new Error(stderr);
+      }
+    
+      const lines = stdout!.split(/\n/);
+      const tunnelName = lines[0].split(" ")[1]?.replace(/(\r\n|\n|\r)/gm, "");
+    
+      return tunnelName;
+    } catch (e) {
+      if (e instanceof ExecError) {
+        if (!e.stderr) {
+          throw new Error(e.stderr);
         }
-        if (stderr) {
-          reject(stderr);
+
+        const splittedText: string[] = String(e.stderr).split(":");
+
+        if (splittedText.length == 0) {
+          return "";
         }
-        exec(wgQuickCommand, (error, _, stderr) => {
-          if (error) {
-            reject(error);
-          }
-          if (stderr) {
-            reject(stderr);
-          }
-          resolve(true);
-        });
-      });
-    });
+
+        const indexOfName = splittedText[0].lastIndexOf(" ");
+        return splittedText[0].substring(indexOfName + 1, splittedText[0].length);
+      }
+      throw e;
+    }
   }
 
-  save(config: WgConfig, dir: string): Promise<void> {
-    const file = `${dir}/${config.name}.conf`;
-    const output = `
-            [Interface]
-            Address = ${config.interface.address}
-            PrivateKey = ${config.interface.address}
-            ListenPort = ${config.interface.listenPort}
-            DNS = ${config.interface.dns}
-            
-            [Peer]\nPublicKey = ${config.peer.publicKey}
-            AllowedIPs = ${config.peer.allowedIPs}
-            Endpoint = ${config.peer.endpoint}
-            PersistentKeepalive = ${config.peer.persistentKeepalive}
-        `;
-    const command = `echo "${output}" > ${file}`;
-    return new Promise(() => exec(command));
+  async up(filePath: string): Promise<void> {
+    await this.exec(`wg-quick up ${filePath.replace(" ", "\\ ")}`);
   }
 
-  up(device: string, dir: string): Promise<void> {
-    const file = `${dir}/${device}.conf`;
-    const command = `wg-quick up ${file}`;
-    return new Promise<void>(() => exec(command));
+  async down(filePath: string): Promise<void> { 
+    await this.exec(`wg-quick down ${filePath.replace(" ", "\\ ")}`);
   }
 
-  down(device: string): Promise<void> {
-    const command = `wg-quick down ${device}`;
-    return new Promise(() => exec(command));
-  }
-
-  status(device: string): Promise<boolean> {
+  async status(device: string): Promise<boolean> {
     const command = `wg show ${device}`;
-    return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-        }
-        if (stderr) {
-          reject(stderr);
-        }
-        resolve(stdout.length > 0);
-      });
-    });
+    const { stderr, stdout } = await this.exec(command);
+    if (stderr) {
+      throw new Error(String(stderr));
+    }
+    return stdout!.match(/interface: (.*)/) ? true : false;
   }
 
-  getPublicKey(privateKey: string): Promise<string> {
+  async generatePrivateKey(): Promise<string> {
+    const {stdout, stderr} = await this.exec('wg genkey', false);
+    if (stderr) {
+      throw new Error(stderr);
+    }
+    return stdout!.trim();
+  }
+
+  async getPublicKey(privateKey: string): Promise<string> {
     const command = `echo "${privateKey}" | wg pubkey`;
-    return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-        }
-        if (stderr) {
-          reject(stderr);
-        }
-        resolve(stdout);
-      });
-    });
+    const { stdout, stderr } = await this.exec(command, false);
+    if (stderr) {
+      throw new Error(stderr);
+    }
+    return stdout!.trim();
   }
 }
